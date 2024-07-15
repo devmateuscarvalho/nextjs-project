14aae71a43443b32628935bef5b25ab4584b18f5
mateus <mateus.carvalho@paylab.com.br>
2024-07-15 11:20:52 -0300

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/:path*',
+          destination: '/pages/:path*',
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
